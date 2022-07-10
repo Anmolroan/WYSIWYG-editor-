af21ff80fc4cee3d3eb8ae0c61a3ba3fce531142
Anmolroan <anmol.kumar@masaischool.com>
2022-07-10 19:30:21 +0530

    first commit

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>Ham</div>
+        <div>search</div>
+        <div> invite Team members</div>
+        <div>notification</div>
+    </div>
+  )
+}
+
+export default Navbar
